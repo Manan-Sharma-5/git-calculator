19459711ea7cb6bdc5c7667277d0d7a1ae2e445e
Manan Sharma <112090668+Manan-Sharma-5@users.noreply.github.com>
2025-11-24 21:08:36 +0530

    Added division feature with modified print statement

--- calculator.py
+++ calculator.py
@@ -1,5 +1,9 @@
 def add(a, b):
     return a + b
 
-print("Calculator Program Started")
+def divide(a, b):
+    return a / b
+
+print("Calculator Program Started - Division Version")
 print("Addition of 5 and 3 =", add(5, 3))
+print("Division of 10 by 2 =", divide(10, 2))
