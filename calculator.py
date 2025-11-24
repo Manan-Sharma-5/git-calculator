98f5a2c38dca1aebb7acf1674d748c8966607429
Manan Sharma <112090668+Manan-Sharma-5@users.noreply.github.com>
2025-11-24 21:07:45 +0530

    Added multiplication feature

--- calculator.py
+++ calculator.py
@@ -1,5 +1,9 @@
 def add(a, b):
     return a + b
 
+def multiply(a, b):
+    return a * b
+
 print("Calculator Program Started")
 print("Addition of 5 and 3 =", add(5, 3))
+print("Multiplication of 5 and 3 =", multiply(5, 3))
